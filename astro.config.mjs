f08c1ad2198062751f277740aafad327c7a08046
Mat6677 <136177955+Mat6677@users.noreply.github.com>
2023-09-06 07:19:53 -0300

    images src/

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://Mat6677.github.io",
+  site: "https://mat6677.github.io",
   base: "/my-portfolio",
 });
